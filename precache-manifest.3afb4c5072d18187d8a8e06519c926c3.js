3dde0fdfbb9691536c8cfa5a8329ce4283a487a9
Musadiq Khan <Musadiq.Khan@gmail.com>
2019-08-28 19:41:37 +0500

    Updates

---
+++ precache-manifest.3afb4c5072d18187d8a8e06519c926c3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f875bc8cbf850b8455fede35c20366ae",
+    "url": "/MussadiqKhan/imageapp/index.html"
+  },
+  {
+    "revision": "27c16ef9a62c47a95a39",
+    "url": "/MussadiqKhan/imageapp/static/css/2.504f1c7f.chunk.css"
+  },
+  {
+    "revision": "268bcd14ff22ff54a5dd",
+    "url": "/MussadiqKhan/imageapp/static/css/main.62173de4.chunk.css"
+  },
+  {
+    "revision": "27c16ef9a62c47a95a39",
+    "url": "/MussadiqKhan/imageapp/static/js/2.43dde73f.chunk.js"
+  },
+  {
+    "revision": "268bcd14ff22ff54a5dd",
+    "url": "/MussadiqKhan/imageapp/static/js/main.1e3bf8f3.chunk.js"
+  },
+  {
+    "revision": "6c1618b17579d8e08fbb",
+    "url": "/MussadiqKhan/imageapp/static/js/runtime~main.13811ebc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/MussadiqKhan/imageapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/MussadiqKhan/imageapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/MussadiqKhan/imageapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/MussadiqKhan/imageapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/MussadiqKhan/imageapp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
